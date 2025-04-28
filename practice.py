160a6c463ee3e228b43edd22675e8ec026f44044
RajMujmer <rajat.mujmer0206@gmail.com>
2025-04-28 15:26:55 +0530

    practice.py

--- practice.py
+++ practice.py
@@ -14,5 +14,6 @@ st.write("Here is the data : ", df)
 st.markdown("# This is a level 1 heading")
 st.text("Ab Samajh aya ki nahi")
 
-age = st.slider("Select your age:", 0, 100, 25, help="This slider controls your age.")
+
 name = st.text_input("Enter your name:", label="Your Name", placeholder="e.g., Charlie")
+age = st.slider("Select your age:", 0, 100, 25, help="This slider controls your age.")
