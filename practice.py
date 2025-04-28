4409a600401089871dae6ce6063a1a3c85ba61a1
RajMujmer <rajat.mujmer0206@gmail.com>
2025-04-28 14:39:49 +0530

    practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 st.title("This is for practice purpose")
 st.header("Exploring Data....")
 st.subheader("Here")
+
+st.write("Dekho!!! Time Ho gaya hai.")
